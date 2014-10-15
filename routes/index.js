ea70bad9a69314dfb0df842a2d10045dd2c41003
jiawen2016 <jiawen2016@gmail.com>
2014-10-15 12:54:54 -0700

    5

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ exports.view = function(req, res){
   res.render('index', {
     'projects': [
       { 'name': 'Ribbon Dance Sound Visualizer',
-        'image': 'screenshot1.png',
+        'image': 'screen5.png',
         'id': 'project1'
       },
       
